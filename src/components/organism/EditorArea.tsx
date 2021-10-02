b5f7c84083923afad5f674958e056fb74e4d043e
hiro <hmaeyama0510@gmail.com>
2021-10-02 09:46:01 +0900

    style: fix the edia area's border #70

--- src/components/organism/EditorArea.tsx
+++ src/components/organism/EditorArea.tsx
@@ -10,17 +10,19 @@ interface Props {}
 const EditorArea = (props: Props) => {
   return (
     <>
-      <div className=' border dark:border-penn-gray dark:bg-penn-darkGray rounded-lg p-4 text-left mb-10 '>
-        <div className='mx-4 my-3 border-b'>
-          <AvatarWithName />
+      <div className=' border dark:border-penn-gray dark:bg-penn-darkGray rounded-lg text-left mb-10 '>
+        <div className='my-3 border-b'>
+          <div className="mt-6 ml-8 ">
+            <AvatarWithName />
+          </div>
             <ul className='flex cursor-pointer mt-4 border-b '>
-              <li className='py-2 px-6 bg-white rounded-t-lg border-b hover:border dark:bg-penn-darkGray hover:border-b-white dark:border-penn-gray  dark:border-b-penn-darkGray'>Write</li>
+              <li className='ml-4 py-2 px-6 bg-white rounded-t-lg border-b hover:border dark:bg-penn-darkGray hover:border-b-white dark:border-penn-gray  dark:border-b-penn-darkGray'>Write</li>
               <li className='py-2 px-6 bg-white rounded-t-lg  border-b hover:border hover:border-b-white text-gray-500 dark:bg-penn-darkGray dark:border-penn-gray dark:border-b-penn-darkGray '>Preview</li>
               <li className='py-2 px-6 bg-white rounded-t-lg border-b hover:border hover:border-b-white text-gray-500 dark:bg-penn-darkGray dark:border-penn-gray dark:border-b-penn-darkGray '>Guide</li>
             </ul>
             <input className='w-[100%] h-[100%] border-none outline-none dark:border-penn-gray dark:bg-penn-darkGray rounded-lg p-4 text-left mb-10 ' type="text"/>
           </div>
-            <div className="flex justify-end gap-4 mr-4">
+            <div className="flex justify-end gap-4 mr-10 mb-4">
               <CloseButton/>
               <SubmitButton/>
             </div>
