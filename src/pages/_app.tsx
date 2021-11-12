8a4df4c508c2c8396bfd51ab95c3e9bee37fab55
hiro <hmaeyama0510@gmail.com>
2021-11-12 16:30:42 +0900

    fix: small change for isLoading style #116

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ const Auth = ({ children }: Props): JSX.Element => {
   return isLoading ? (
     <>
       <div className='fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center'>
-        <div className='animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-600'></div>
+        <div className='animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-600'></div>
       </div>
     </>
   ) : (
