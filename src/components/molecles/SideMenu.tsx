65a82f19327ddd3e9b0908b41ac23792d5a25258
hiro <hmaeyama0510@gmail.com>
2021-11-15 11:41:15 +0900

    func: change Link component to fix issue #128

--- src/components/molecles/SideMenu.tsx
+++ src/components/molecles/SideMenu.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 interface Props {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const SideMenu = ({children}: Props) => {
+// eslint-disable-next-line react/display-name
+const SideMenu = ({ children }: Props) => {
+  // const SideMenu = React.forwardRef(({children, href}:any,ref:any) => {
   return (
     <>
-      <div className='flex gap-2 cursor-pointer'>
-        {children}
-      </div>
+      <div className='flex gap-2 cursor-pointer'>{children}</div>
     </>
   );
 };
